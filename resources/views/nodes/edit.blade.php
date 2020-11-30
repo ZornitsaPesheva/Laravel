df86780d4ccc4099b6ebabc554aa7c990a34eccb
zornitsaserbezova <zornitsa.serbezova@balkangraph.com>
2020-11-30 17:39:16 +0200

    edit edited

--- resources/views/nodes/edit.blade.php
+++ resources/views/nodes/edit.blade.php
@@ -1,13 +1,13 @@
-@extends('products.layout')
+@extends('nodes.layout')
    
 @section('content')
     <div class="row">
         <div class="col-lg-12 margin-tb">
             <div class="pull-left">
-                <h2>Edit Product</h2>
+                <h2>Edit Node</h2>
             </div>
             <div class="pull-right">
-                <a class="btn btn-primary" href="{{ route('products.index') }}"> Back</a>
+                <a class="btn btn-primary" href="{{ route('nodes.index') }}"> Back</a>
             </div>
         </div>
     </div>
@@ -23,21 +23,21 @@
         </div>
     @endif
   
-    <form action="{{ route('products.update',$product->id) }}" method="POST">
+    <form action="{{ route('noded.update',$node->id) }}" method="POST">
         @csrf
         @method('PUT')
    
          <div class="row">
             <div class="col-xs-12 col-sm-12 col-md-12">
                 <div class="form-group">
-                    <strong>Name:</strong>
-                    <input type="text" name="name" value="{{ $product->name }}" class="form-control" placeholder="Name">
+                    <strong>Id:</strong>
+                    <input type="text" name="id" value="{{ $node->name }}" class="form-control" placeholder="Id">
                 </div>
             </div>
             <div class="col-xs-12 col-sm-12 col-md-12">
                 <div class="form-group">
-                    <strong>Detail:</strong>
-                    <textarea class="form-control" style="height:150px" name="detail" placeholder="Detail">{{ $product->detail }}</textarea>
+                    <strong>reportsTo:</strong>
+                    <textarea class="form-control" style="height:150px" name="reportsTo" placeholder="ReportsTo">{{ $node->detail }}</textarea>
                 </div>
             </div>
             <div class="col-xs-12 col-sm-12 col-md-12 text-center">
