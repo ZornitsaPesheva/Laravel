e9760b5e2ea7458702920396b93267b42e8db814
zornitsaserbezova <zornitsa.serbezova@balkangraph.com>
2020-11-30 18:24:53 +0200

    hardcoded chart added

--- resources/views/nodes/index.blade copy.php
+++ resources/views/nodes/index.blade copy.php
@@ -46,19 +46,7 @@
         @endforeach
     </table>
 
-    <div id="tree">Chart here</div>
-    <script>
-        var chart = new OrgChart(document.getElementById("tree"), {
-            nodeBinding: {
-                field_0: "name"
-            },
-            nodes: [
-                { id: 1, name: "Amber McKenzie" },
-                { id: 2, pid: 1, name: "Ava Field" },
-                { id: 3, pid: 1, name: "Peter Stevens" }
-            ]
-        });
-    </script>
+
   
     {!! $nodes->links() !!}
       
